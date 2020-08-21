ead618df2759e194a916b4d5ccc570c1865de332
DKilchik <dmitriy.kylchik@instatsport.com>
2020-08-21 14:41:31 +0300

    db connect updated

--- demo/views/frontend.py
+++ demo/views/frontend.py
@@ -1,8 +1,17 @@
 import aiohttp
 from aiohttp_jinja2 import template
+from sqlalchemy.sql import text
 
 
 @template('index.html')
 async def index(request):
     site_name = request.app['config'].get('site_name')
     return {'site_name': site_name}
+
+
+async def post(request):
+    async with request.app['db'].acquire() as conn:
+        query = text('Select * from post;')
+        result = await conn.fletch(query)
+
+    return aiohttp.web.Response(body=str(result))
